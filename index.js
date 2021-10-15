86a253d6c426e4a1002398120614bf869f7a5179
Aman <8211amanverma@gmail.com>
2021-10-15 16:07:34 +0530

    update

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-// // back tot top
 
-// let backToTopBtn = document.querySelector('.back-to-top')
 
-// window.onscroll = () => {
-//     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-//         backToTopBtn.style.display = 'flex'
-//     } else {
-//         backToTopBtn.style.display = 'none'
-//     }
-// }
 
-// top nav menu
 
 let menuItems = document.getElementsByClassName('menu-item')
 
@@ -22,7 +12,7 @@ Array.from(menuItems).forEach((item, index) => {
     }
 })
 
-//  category
+
 
 let foodMenuList = document.querySelector('.food-item-wrap')
 
@@ -39,7 +29,7 @@ Array.from(categories).forEach((item, index) => {
     }
 })
 
-// on scroll animation
+
 
 let scroll = window.requestAnimationFrame || function(callback) {window.setTimeout(callback, 1000/60)}
 
@@ -71,7 +61,6 @@ loop = () => {
 
 loop()
 
-// mobile nav
 
 let bottomNavItems = document.querySelectorAll('.mb-nav-item')
 
